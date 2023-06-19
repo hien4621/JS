380e91b11553dde8d4c71b35fc3cd4f7d9f26b77
hien4621 <hien4621@gmail.com>
2023-06-19 16:34:36 +0700

    1.2

--- src/level1/1.2.test.js
+++ src/level1/1.2.test.js
@@ -1,7 +1,27 @@
 const lengthstr = require('./1.2.js');
 
 test('test1', () => {
-//   const str = "hsuyyyyy5";
+  const str = "Hi everyone";
   const  lengthnum= lengthstr(str);
-  expect(lengthnum).toEqual(9);
+  expect(lengthnum).toEqual(11);
+}); 
+test('test2', () => {
+  const str = "   Have a good day!   ";
+  const  lengthnum= lengthstr(str);
+  expect(lengthnum).toEqual(22);
+}); 
+test('test3', () => {
+  const str = "";
+  const  lengthnum= lengthstr(str);
+  expect(lengthnum).toEqual(0);
+}); 
+test('test4', () => {
+  const str = "12345";
+  const  lengthnum= lengthstr(str);
+  expect(lengthnum).toEqual(5);
+}); 
+test('test5', () => {
+  const str = "   ";
+  const  lengthnum= lengthstr(str);
+  expect(lengthnum).toEqual(3);
 }); 
