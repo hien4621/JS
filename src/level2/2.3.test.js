88d653c647c08539cac978d54185f87d68998a70
hien4621 <hien4621@gmail.com>
2023-06-20 14:37:29 +0700

    2.3

---
+++ src/level2/2.3.test.js
@@ -0,0 +1,24 @@
+const longestcommon = require('./2.3.js');
+
+test('test 1', () => {
+    const list = ['hi', 'everyone'];
+    expect(longestcommon(list)).toEqual(-1);
+});
+
+test('test 2', () => {
+    const list = ['apple', 'potato'];
+    expect(longestcommon(list)).toEqual('a');
+});
+test('test 3', () => {
+    const list = ['have', 'ave'];
+    expect(longestcommon(list)).toEqual('ave');
+});
+test('test 4', () => {
+    const list = ['',''];
+    expect(longestcommon(list)).toEqual(-1);
+});
+test('test 5', () => {
+    const list = ['    ', 'hello   '];
+    expect(longestcommon(list)).toEqual('   ');
+});
+
